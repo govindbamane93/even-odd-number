ae2d8fd031622b91c518afd7023e44ccc843c8ca
govindbamane93 <125502752+govindbamane93@users.noreply.github.com>
2023-02-16 01:08:44 +0530

    Add files via upload

---
+++ IF_ELSE_CONDITION.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+num=int(input('enter the number'))
+if num%2==0:
+    print(num,'num is even number')
+          
+else:
+          print(num,'number is odd')
+          
+          
+
+
+# In[ ]:
+
+
+
+
